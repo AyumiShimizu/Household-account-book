9a6bf41958cbe64952c7caa4d73b0da05db99eef
AyumiShimizu <platonic0801@gmail.com>
2019-11-14 12:47:36 +0900

    Add sidebar script by php

---
+++ sidebar.php
@@ -0,0 +1,11 @@
+    <aside id="sidebar">
+      <ul class="side-nav">
+        <li><a href="totalMonth.php">集計一覧</a></li>
+        <li><a href="slipList.php">入出金一覧</a></li>
+        <li><a href="categoryEdit.php">カテゴリー編集</a></li>
+        <li><a href="bankEdit.php">口座編集</a></li>
+        <li><a href="profEdit.php">プロフィール編集</a></li>
+        <li><a href="passEdit.php">パスワード編集</a></li>
+        <li><a href="withdraw.php">退会</a></li>
+      </ul>
+    </aside>
